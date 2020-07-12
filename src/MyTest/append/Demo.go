be2a2cd1ed3eb96c7c43781e98ffa60f5a3b35aa
yida <zhangyfei126@yeah.net>
2020-07-12 12:04:02 +0800

    重新调整图片生成

---
+++ src/MyTest/append/Demo.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var a = make([]int, 3)
+	a[0] = 2
+	a[1] = 3
+	a[2] = 4
+	fmt.Println(a)
+	fmt.Println(reflect.TypeOf(a))
+
+	var b = make([]int, 2)
+	b[0] = 20
+	b[1] = 21
+
+	ints := append(b, a...)
+	fmt.Println(ints)
+
+}
