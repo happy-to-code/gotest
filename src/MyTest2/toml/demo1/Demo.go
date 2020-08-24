bee4a0c293f28a98519def2d132a8bb8ba4ae832
yida <zhangyfei126@yeah.net>
2020-08-25 00:13:20 +0800

    uintToByte

---
+++ src/MyTest2/toml/demo1/Demo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+)
+
+type Dict struct {
+	Name     string
+	Code     int
+	Describe string
+}
+type Dicts struct {
+	DictList []Dict
+}
+
+func main() {
+	var dicts Dicts
+
+	if _, err := toml.DecodeFile("135.toml", &dicts); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("dicts:%+v\n", dicts)
+
+	for _, dict := range dicts.DictList {
+		fmt.Println(dict.Name)
+		fmt.Println(dict.Code)
+		fmt.Println(dict.Describe)
+	}
+
+}
---
+++ src/MyTest2/toml/demo2/Demo.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+)
+
+type item struct {
+	ItemName   string
+	ItemDesc   string
+	Attributes attributes
+	Part       []part
+}
+
+type part struct {
+	PartName string
+	PartLink string
+}
+
+type attributes struct {
+	Material string
+	Light    string
+	TransPC  string
+	Displace string
+}
+
+type items struct {
+	Item []item
+}
+
+func main1() {
+
+	var allitems items
+
+	if _, err := toml.DecodeFile("123.toml", &allitems); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("\n")
+
+	for _, items := range allitems.Item {
+		fmt.Printf("    Item Name: %s \n", items.ItemName)
+		fmt.Printf("  Description: %s \n\n", items.ItemDesc)
+
+		fmt.Printf("     Material: %s \n", items.Attributes.Material)
+		fmt.Printf("     Lightmap: %s \n", items.Attributes.Light)
+		fmt.Printf(" TL Precision: %s \n", items.Attributes.TransPC)
+		fmt.Printf("   DP Channel: %s \n", items.Attributes.Displace)
+
+		fmt.Printf("    Part Name: %s \n", items.Part[0].PartName)
+		fmt.Printf("    Part Link: %s \n", items.Part[0].PartLink)
+
+		fmt.Printf("\n────────────────────────────────────────────────────┤\n\n")
+
+	}
+
+	fmt.Printf("\n")
+
+}
