b29dd1995c8d664c433838ca80cdf49d6743004b
yida <zhangyfei126@yeah.net>
2020-08-31 23:04:18 +0800

    go-tour

---
+++ src/go-tour/blok-service/go.mod
@@ -0,0 +1,16 @@
+module github.com/blok-service
+
+go 1.14
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/spf13/viper v1.7.1
+	golang.org/x/sys v0.0.0-20200828194041-157a740278f4 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
--- src/go-tour/blok-service/main.go
+++ src/go-tour/blok-service/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/blok-service/global"
 	"github.com/blok-service/internal/routers"
+	"github.com/blok-service/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
@@ -9,11 +13,47 @@ import (
 func main() {
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20, // 1048576
 	}
+
+	fmt.Println("1111:::==>", global.ServerSetting.HttpPort)
+	fmt.Println("2222:::==>", global.DatabaseSetting.Password)
 	s.ListenAndServe()
+}
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v\n", err)
+	}
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	return nil
 }
